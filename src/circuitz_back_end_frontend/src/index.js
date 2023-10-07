c9ef9b0f62d058231b70ed617a467888d6617eb1
Slmii <selami.c@sbytes-it.com>
2023-10-07 15:30:15 +0200

    Initial commit.

---
+++ src/circuitz_back_end_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { circuitz_back_end_backend } from "../../declarations/circuitz_back_end_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await circuitz_back_end_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
